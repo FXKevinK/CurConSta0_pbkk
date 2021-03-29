4b55a4bee665ed6d3009ecf19fe48673ceb07837
F. X. Kevin Koesnadi <58078219+FXKevinK@users.noreply.github.com>
2021-03-29 09:42:02 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CurConSta0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.NumericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(183, 382);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(186, 31);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(114, 305);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 44);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "USD",
+            "SGD",
+            "EUR",
+            "JPY",
+            "MYR",
+            "IDR"});
+            this.comboBox1.Location = new System.Drawing.Point(183, 164);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 33);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "IDR",
+            "USD",
+            "EUR",
+            "SGD",
+            "JPY",
+            "MYR"});
+            this.comboBox2.Location = new System.Drawing.Point(183, 236);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(186, 33);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // NumericUpDown1
+            // 
+            this.NumericUpDown1.Location = new System.Drawing.Point(183, 97);
+            this.NumericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.NumericUpDown1.Name = "NumericUpDown1";
+            this.NumericUpDown1.Size = new System.Drawing.Size(186, 31);
+            this.NumericUpDown1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(47, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Static Currency Converter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(61, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(80, 236);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "To";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(24, 104);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Amount (From)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(24, 389);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Converted Amount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 456);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.NumericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sta. Currency Calc.";
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.NumericUpDown NumericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurConSta0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rate = int.Parse(NumericUpDown1.Text);
+            float result = (float)rate;
+            // idr
+            if ((string)comboBox1.SelectedItem == "IDR" && (string)comboBox2.SelectedItem == "IDR")
+            {
+                result = (float)rate * 1;
+            }
+            else if ((string)comboBox1.SelectedItem == "IDR" && (string)comboBox2.SelectedItem == "USD")
+            {
+                result = (float)rate / 14415;
+            }
+            else if ((string)comboBox1.SelectedItem == "IDR" && (string)comboBox2.SelectedItem == "EUR")
+            {
+                result = (float)rate / 17045;
+            }
+            else if ((string)comboBox1.SelectedItem == "IDR" && (string)comboBox2.SelectedItem == "SGD")
+            {
+                result = (float)rate / 10710;
+            }
+            else if ((string)comboBox1.SelectedItem == "IDR" && (string)comboBox2.SelectedItem == "JPY")
+            {
+                result = (float)rate / (float)130.89;
+            }
+            else if ((string)comboBox1.SelectedItem == "IDR" && (string)comboBox2.SelectedItem == "MYR")
+            {
+                result = (float)rate / 3474;
+            }
+
+            // usd
+            else if ((string)comboBox1.SelectedItem == "USD" && (string)comboBox2.SelectedItem == "USD")
+            {
+                result = (float)rate * 1;
+            }
+            else if ((string)comboBox1.SelectedItem == "USD" && (string)comboBox2.SelectedItem == "IDR")
+            {
+                result = (float)rate * 14415;
+            }
+            else if ((string)comboBox1.SelectedItem == "USD" && (string)comboBox2.SelectedItem == "EUR")
+            {
+                result = (float)rate / (float)1.18;
+            }
+            else if ((string)comboBox1.SelectedItem == "USD" && (string)comboBox2.SelectedItem == "SGD")
+            {
+                result = (float)rate * (float)1.35;
+            }
+            else if ((string)comboBox1.SelectedItem == "USD" && (string)comboBox2.SelectedItem == "JPY")
+            {
+                result = (float)rate * (float)109.61;
+            }
+            else if ((string)comboBox1.SelectedItem == "USD" && (string)comboBox2.SelectedItem == "MYR")
+            {
+                result = (float)rate * (float)4.15;
+            }
+
+            // eur
+            else if ((string)comboBox1.SelectedItem == "EUR" && (string)comboBox2.SelectedItem == "EUR")
+            {
+                result = (float)rate * 1;
+            }
+            else if ((string)comboBox1.SelectedItem == "EUR" && (string)comboBox2.SelectedItem == "IDR")
+            {
+                result = (float)rate * 17045;
+            }
+            else if ((string)comboBox1.SelectedItem == "EUR" && (string)comboBox2.SelectedItem == "USD")
+            {
+                result = (float)rate * (float)1.18;
+            }
+            else if ((string)comboBox1.SelectedItem == "EUR" && (string)comboBox2.SelectedItem == "SGD")
+            {
+                result = (float)rate * (float)1.59;
+            }
+            else if ((string)comboBox1.SelectedItem == "EUR" && (string)comboBox2.SelectedItem == "JPY")
+            {
+                result = (float)rate * (float)129.61;
+            }
+            else if ((string)comboBox1.SelectedItem == "EUR" && (string)comboBox2.SelectedItem == "MYR")
+            {
+                result = (float)rate * (float)4.90;
+            }
+
+
+            // sgd
+            else if ((string)comboBox1.SelectedItem == "SGD" && (string)comboBox2.SelectedItem == "SGD")
+            {
+                result = (float)rate * 1;
+            }
+            else if ((string)comboBox1.SelectedItem == "SGD" && (string)comboBox2.SelectedItem == "IDR")
+            {
+                result = (float)rate * (float)10710;
+            }
+            else if ((string)comboBox1.SelectedItem == "SGD" && (string)comboBox2.SelectedItem == "USD")
+            {
+                result = (float)rate / (float)1.35;
+            }
+            else if ((string)comboBox1.SelectedItem == "SGD" && (string)comboBox2.SelectedItem == "EUR")
+            {
+                result = (float)rate / (float)1.59;
+            }
+            else if ((string)comboBox1.SelectedItem == "SGD" && (string)comboBox2.SelectedItem == "JPY")
+            {
+                result = (float)rate * (float)81.44;
+            }
+            else if ((string)comboBox1.SelectedItem == "SGD" && (string)comboBox2.SelectedItem == "MYR")
+            {
+                result = (float)rate * (float)3.08;
+            }
+
+            // jpy
+            else if ((string)comboBox1.SelectedItem == "JPY" && (string)comboBox2.SelectedItem == "JPY")
+            {
+                result = (float)rate * 1;
+            }
+            else if ((string)comboBox1.SelectedItem == "JPY" && (string)comboBox2.SelectedItem == "IDR")
+            {
+                result = (float)rate * (float)130.89;
+            }
+            else if ((string)comboBox1.SelectedItem == "JPY" && (string)comboBox2.SelectedItem == "USD")
+            {
+                result = (float)rate / (float)109.61;
+            }
+            else if ((string)comboBox1.SelectedItem == "JPY" && (string)comboBox2.SelectedItem == "EUR")
+            {
+                result = (float)rate / (float)129.61;
+            }
+            else if ((string)comboBox1.SelectedItem == "JPY" && (string)comboBox2.SelectedItem == "SGD")
+            {
+                result = (float)rate / (float)81.44;
+            }
+            else if ((string)comboBox1.SelectedItem == "JPY" && (string)comboBox2.SelectedItem == "MYR")
+            {
+                result = (float)rate / (float)26.55;
+            }
+
+            // myr
+            else if ((string)comboBox1.SelectedItem == "MYR" && (string)comboBox2.SelectedItem == "MYR")
+            {
+                result = (float)rate * 1;
+            }
+            else if ((string)comboBox1.SelectedItem == "MYR" && (string)comboBox2.SelectedItem == "IDR")
+            {
+                result = (float)rate * 3474;
+            }
+            else if ((string)comboBox1.SelectedItem == "MYR" && (string)comboBox2.SelectedItem == "USD")
+            {
+                result = (float)rate / (float)4.15;
+            }
+            else if ((string)comboBox1.SelectedItem == "MYR" && (string)comboBox2.SelectedItem == "EUR")
+            {
+                result = (float)rate / (float)4.90;
+            }
+            else if ((string)comboBox1.SelectedItem == "MYR" && (string)comboBox2.SelectedItem == "SGD")
+            {
+                result = (float)rate / (float)3.08;
+            }
+            else if ((string)comboBox1.SelectedItem == "MYR" && (string)comboBox2.SelectedItem == "JPY")
+            {
+                result = (float)rate * (float)26.55;
+            }
+
+            textBox1.Text = result.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
